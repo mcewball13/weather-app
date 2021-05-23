a03258e2c4fc8dc6c4b0cf2a82a18e91d2d06844
Mike <mcewen1984@hotmail.com>
2021-05-23 16:45:30 -0700

    added true and false for time to match 14:00:00

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -1,8 +1,20 @@
 var recentSearchArr = [];
-var currentDate = moment();
+var currentDate = new Date();
 console.log(currentDate);
-
+var duplicate = false;
+function loadRecent() {
+    var loadedRecent = JSON.parse(localStorage.getItem("recentCities"));
+    console.log(loadedRecent);
+    $(loadedRecent).each(function (index, value) {
+        console.log("Thsi is the value " + value);
+        saveRecent(value);
+    });
+}
+// inital call to load dom elements
+loadRecent();
 function getWeather() {
+    // reinitiallize duplicate to false to check to see if there are dups
+    duplicate = false;
     var searchCity = $("#searchCity").val();
     saveRecent(searchCity);
 
@@ -19,18 +31,19 @@ function getWeather() {
             let currentTime = "14:00:00";
             let dtText = response.list[0].dt_txt.split(" ")[1];
             let localTime = parseInt(dtText) - 7;
-            console.log("this is local time " + localTime);
-            console.log("this is the dt text " + dtText);
-            console.log("api response", response);
+            console.log("this is the dtText" + dtText);
+            console.log("dtText is equal to 14 " + (dtText === currentTime));
         });
 }
 $("#button-addon2").on("click", function () {
     getWeather();
+    $("#searchCity").val("").trigger("focus");
 });
 $("#recentSearches").on("click", ".btn", function (btn) {
     var clickedRecentCity = $(this).val();
     $("#searchCity").val(clickedRecentCity);
     getWeather();
+    $("#searchCity").val("").trigger("focus");
 });
 
 function saveRecent(cityName) {
@@ -39,7 +52,8 @@ function saveRecent(cityName) {
         value: cityName,
         class: "btn btn-lg",
     });
-    var duplicate = verifyNoDup(cityName);
+    verifyNoDup(cityName);
+
     console.log("are there any duplicates" + duplicate);
     if (!duplicate) {
         $("#recentSearches").append(recentSearchBtn);
@@ -50,11 +64,11 @@ function saveRecent(cityName) {
 
     function verifyNoDup(cityName) {
         for (var i = 0; i < recentSearchArr.length; i++) {
-            console.log("recent search item" + recentSearchArr[i]);
+            console.log("recent search item " + recentSearchArr[i]);
             if (cityName === recentSearchArr[i] || cityName == "undefined") {
-                return true;
+                return (duplicate = true);
             } else {
-                return false;
+                dublicate = false;
             }
         }
     }
